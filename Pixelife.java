8c70575587954e3c7cb346946de2c5a7aef366cf
Nic Manoogian <zimmoz3@verizon.net>
2013-03-31 16:36:40 -0400

    Added random pixel generation method

--- Pixelife.java
+++ Pixelife.java
@@ -14,13 +14,27 @@ import javax.swing.JPanel;
 public class Pixelife extends JPanel
 {
 	private BufferedImage canvas;
-
-	public Pixelife(int w, int h)
+	private int width;
+	private int height;
+	/**
+	 * Constructor with width and height
+	 * @param w width in pixels
+	 * @param h height in pixels
+	 * @param n number of random pixels to generate
+	 */
+	public Pixelife(int w, int h, int n)
 	{
 		canvas = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+		width = w;
+		height = h;
 		fillCanvas(Color.WHITE);
+		generatePixels(n);
 	}
 
+	/**
+	 * Fills the canvas with a color
+	 * @param c color
+	 */
 	public void fillCanvas(Color c)
 	{
 		int color = c.getRGB();
@@ -46,20 +60,63 @@ public class Pixelife extends JPanel
 		g2.drawImage(canvas, null, null);
 	}
 
+	/**
+	 * Creates a set number pixels
+	 * @param numPixels number of pixels
+	 */
+	public void generatePixels(int numPixels)
+	{
+		System.out.println("Generating Pixels");
+
+		for (int i = 0; i < numPixels; i++)
+		{
+			//Choose some random colors
+			int red = (int)(Math.random() * 256);
+			int green = (int)(Math.random() * 256);
+			int blue = (int)(Math.random() * 256);
+
+			//Choose some random positions
+			int x = (int)(Math.random()*width);
+			int y = (int)(Math.random()*height);
+
+
+			//Make sure locations are different
+			while (canvas.getRGB(x,y) != (Color.WHITE).getRGB())
+			{
+				x = (int)(Math.random()*width);
+				y = (int)(Math.random()*height);
+			}
+
+
+			//Make sure colors are different
+			while (red == green || green == blue || blue == red)
+			{
+				red = (int)(Math.random() * 256);
+				green = (int)(Math.random() * 256);
+				blue = (int)(Math.random() * 256);
+			}
+
+			//set canvas
+			canvas.setRGB(x, y, (new Color(red,green,blue)).getRGB());
+		}
+
+	}
+
 
 	public static void main(String [] args)
 	{
 		int width = 640;
-        int height = 480;
-        JFrame frame = new JFrame("test");
+		int height = 480;
+		JFrame frame = new JFrame("test");
 
-        Pixelife pix = new Pixelife(width, height);
+		Pixelife pix = new Pixelife(width, height, 100000);
 
-        frame.add(pix);
-        frame.pack();
-        frame.setVisible(true);
-        frame.setResizable(false);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		frame.add(pix);
+		frame.pack();
+		frame.setVisible(true);
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 	}
 
