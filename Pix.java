0edb84a39446997be186e278d955147c2f7a4c7e
Nic Manoogian <zimmoz3@verizon.net>
2013-04-01 22:47:28 -0400

    Added Pix interact funtion

--- Pix.java
+++ Pix.java
@@ -1,6 +1,6 @@
 /**
  * Pixel Class
- * 
+ * with Red Green and Blue streams
  */
 public class Pix
 {
@@ -13,6 +13,37 @@ public class Pix
 		red = r;
 		green = g;
 		blue = b;
+	}
+
+	public Pix()
+	{
+		int r = (int)(Math.random() * 256);
+		int g = (int)(Math.random() * 256);
+		int b = (int)(Math.random() * 256);
+
+		while (r == g || g == b || b == r)
+		{
+			r = (int)(Math.random() * 256);
+			g = (int)(Math.random() * 256);
+			b = (int)(Math.random() * 256);
+		}
+	}
+
+	public void set(int r, int g, int b)
+	{
+		red = r;
+		green = g;
+		blue = b;
+	}
+
+	public void getDomChannel()
+	{
+
+	}
+
+	public void interact(Pix p)
+	{
+
 	}
 
 }
