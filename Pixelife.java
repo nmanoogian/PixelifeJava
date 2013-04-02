2226bad54c3b6fee39b1e55326e7d07813f7e186
Nic Manoogian <zimmoz3@verizon.net>
2013-04-01 22:26:16 -0400

    Added some interaction

--- Pixelife.java
+++ Pixelife.java
@@ -97,9 +97,51 @@ public class Pixelife extends JPanel
 			}
 
 			//set canvas
-			canvas.setRGB(x, y, (new Color(red,green,blue)).getRGB());
+			setPixel(x, y, red, green, blue);
+			//canvas.setRGB(x, y, (new Color(red,green,blue)).getRGB());
 		}
 
+	}
+
+
+	/**
+	 * 
+	 * @param i row
+	 * @param j column
+	 * @param r red integer
+	 * @param b blue integer
+	 * @param g green integer
+	 */
+	public void setPixel(int i, int j, int r, int g, int b)
+	{
+		canvas.setRGB(i, j, (new Color(r,g,b)).getRGB());
+	}
+
+	/**
+	 * Determine if a pixel is in bounds
+	 * @param i row
+	 * @param j column
+	 */
+	public boolean inBounds(int i, int j)
+	{
+		return (i >= 0 && i < height && j >= 0 && j < width);
+	}
+
+	/**
+	 * Determine if a pixel is in bounds and non-white
+	 * @param i row
+	 * @param j column
+	 */
+	public boolean isPixel(int i, int j)
+	{
+		if (inBounds(i,j))
+		{
+			return (canvas.getRGB(i, j) != (Color.WHITE).getRGB());
+		}
+		else
+		{
+			return false;
+		}
 	}
 
 	/**
@@ -112,23 +154,40 @@ public class Pixelife extends JPanel
 		{
 			for (int j = 0; j < height; j++)
 			{
-				if (canvas.getRGB(i, j) != (Color.WHITE).getRGB())
+				if (isPixel(i,j))
 				{
+					//Select a direction and calculate coordinates
 					int dir = (int)(Math.random()*4);
+					int interi = i;
+					int interj = j;
 					if (dir == 0)
 					{
-
+						interi--;
 					}
 					else if (dir == 1)
 					{
+						interi++;
 
 					}
 					else if (dir == 2)
 					{
-
+						interj--;
 					}
 					else
 					{
+						interj++;
+					}
+
+					if (isPixel(interi, interj))
+					{
+						//Interact with pixel
+					}
+					else 
+					{
+						if (inBounds(interi,interj))
+						{
+							canvas.setRGB();
+						}
 
 					}
 				}
@@ -161,7 +220,7 @@ public class Pixelife extends JPanel
 		frame.setVisible(true);
 		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		pix.run();
+		//pix.run();
 
 
 
