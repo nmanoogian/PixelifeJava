84714c34729d29124bbe4d0011be82a479a90477
Mike Lyons <mdl0394@gmail.com>
2013-04-01 22:44:20 -0400

    Added constructors

--- PixGrid.java
+++ PixGrid.java
@@ -4,4 +4,18 @@ public class PixGrid
 {
 
 	private Pix[][] grid;
+
+	public PixGrid()
+	{
+		// Defaults to 640x480
+		grid = new Pix[640][480]
+	}
+
+	public PixGrid(int xsize, int ysize)
+	{
+		grid = new Pix[xsize][ysize];
+	}
+
+
+	
 }
